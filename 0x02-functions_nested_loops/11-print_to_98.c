554e68885cb97f6e6bc490dfadde6f8e517fb23f
Tradix <tradix2021@gmail.com>
2023-09-12 14:18:26 -0700

    From Groot again

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,46 +1,24 @@
-#include "main.h"
+#include"main.h"
+#include <stdio.h>
+
 /**
- * print_to_98 - no brainer, prints values
+ * print_to_98 - print n to 98 counts
+ *           separated by comma, followed
+ *           by space and number should be
+ *           printed in order
  *
- * @n: parameter value
- *
- * Return: void
- */
+ * @n: input
+*/
+
 void print_to_98(int n)
 {
-	if (n < 98)
-	{
-		while (n < 98)
-		{
-			if (n > 9)
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
-			n++;
-		}
-	}
-	else if (n > 98)
-	{
-		while (n > 98)
-		{
-			if (n > 9)
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
-			n--;
-		}
-	}
-	else
-	{
-		int digit = 98;
+	int count;
 
-		_putchar(digit + '0');
-	}
-	_putchar('\n');
+	if (n > 98)
+		for (count = n; count > 98; --count)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
+	printf("98\n");
 }
