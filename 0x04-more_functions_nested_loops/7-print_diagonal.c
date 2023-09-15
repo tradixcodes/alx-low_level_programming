83d9ac53196575f3a80bc996c12eb27eff747610
Tradix <tradix2021@gmail.com>
2023-09-15 16:08:46 -0700

    Mine 2

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,8 +9,6 @@
 
 void print_diagonal(int n)
 {
-	int n, space;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
@@ -22,12 +20,13 @@ void print_diagonal(int n)
 		for (i = 0; i < n; i++)
 		{
 			int j = 0;
-			
+/*			*/
 			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
-
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
+}
