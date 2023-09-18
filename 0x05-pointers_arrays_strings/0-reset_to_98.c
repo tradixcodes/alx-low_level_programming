95985e5924cd39d575788d45fffe9b728acd36e0
Tradix <tradix2021@gmail.com>
2023-09-18 14:01:03 -0700

    stressss

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to reset using pointers
+ *
+ * @n: pointer declared
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	int i;
+
+	*n = 98;
+	i = *n;
+	_putchar(i + 48);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x05. C - Pointers, arrays and strings <br>
+## Intro <br>
+- Will include documentation and links soon<br>
