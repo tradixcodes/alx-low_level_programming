f2089319984354d07ce58391435fc458ce5add41
Tradix <tradix2021@gmail.com>
2023-09-18 15:31:02 -0700

    criteria

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - prints string under certain criteria
+ * for instance here it only prints even array indices
+ * @str: parameter that allows both char and char[]
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] = '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(s[i]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
