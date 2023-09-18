066d370373f36edbeb32f33e39009854d6c35ff8
Tradix <tradix2021@gmail.com>
2023-09-18 14:36:47 -0700

    Strlen I

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - counts characters
+ * can't believe we are using pointers
+ * @s: parameter for a string
+ *
+ * Return: integer size of the string
+ */
+int _strlen(char *s)
+{
+	int len = strlen(s);
+
+	return (len);
+}
