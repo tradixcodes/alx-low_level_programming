fd71a54846d1498046ec0bbe5db7e14166d114fc
Tradix <tradix2021@gmail.com>
2023-09-18 14:10:59 -0700

    Swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps integers
+ *
+ * @a: parameter value for address
+ * @b: same as above
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
