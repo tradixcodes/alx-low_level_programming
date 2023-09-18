4941d9044210c000b417ad71595251e37b298c29
Tradix <tradix2021@gmail.com>
2023-09-18 16:54:35 -0700

    arrays V

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,16 +12,23 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		if (i < (n - 1))
+		break;
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			printf("%d", a[i]);
-			printf(", ");
-		}
-		else
-		{
-			printf("%d\n", a[i]);
+			if (i < (n - 1))
+			{
+				printf("%d", a[i]);
+				printf(", ");
+			}
+			else
+			{
+				printf("%d\n", a[i]);
+			}
 		}
 	}
 }
