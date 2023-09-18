99d2d885f75f5fa66e9c47576e1847e751fdb9f0
Tradix <tradix2021@gmail.com>
2023-09-18 15:01:24 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints in reverse
+ *
+ * @s: parameter for a string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s); i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
