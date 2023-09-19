b171ebff0b757f766d197fe1b591297b712af0f6
Tradix <tradix2021@gmail.com>
2023-09-18 17:05:21 -0700

    cpy I

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy - copies strings
+ *
+ * @dest: destination parameter
+ * @src: source parameter
+ *
+ * Return: Destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
