792912689df369ea8e3a0231e72ad22d77742779
Tradix <tradix2021@gmail.com>
2023-09-19 08:07:20 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _atoi - ASCII to Integer
+ * strtol is better than atoi in a way
+ * @s: parameter for a string
+ * Return: the ASCII value of the string
+ */
+int _atoi(char *s)
+{
+	int num = atoi(s);
+
+	return (num);
+}
