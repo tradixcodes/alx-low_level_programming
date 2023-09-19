a5f2b2ee8ef06eb9d8fbf0467960c62cdf0d1c20
Tradix <tradix2021@gmail.com>
2023-09-19 08:20:41 -0700

    atoi II

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,28 @@
  */
 int _atoi(char *s)
 {
-	int num = atoi(s);
+	int num;
 
+	if (*s == '-')
+	{
+		/*Negative number*/
+		num = atoi(s + 1);
+		num = -num;
+	}
+	else if (*s == '+')
+	{
+		/*Positive number*/
+		num = atoi(s + 1);
+	}
+	else if (isdigit(*s))
+	{
+		/*No sign, assume positive*/
+		num = atoi(s);
+	}
+	else
+	{
+		/*Invalid input*/
+		return (0);
+	}
 	return (num);
 }
