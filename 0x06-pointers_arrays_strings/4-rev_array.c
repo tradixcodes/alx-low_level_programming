119ed0b0ae2d79fd6bda11a17eb2b7427baeda4b
Tradix <tradix2021@gmail.com>
2023-09-21 12:23:38 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - function that prints arrays
+ * inversely
+ * @a: array parameter
+ * @n: no of items in the array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n; i >= 0; i--)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+			printf("%d", a[i]);
+		}
+		else/* */
+		{
+			printf(" ");
+			printf("%d", a[i]);
+		}
+	}
+}
