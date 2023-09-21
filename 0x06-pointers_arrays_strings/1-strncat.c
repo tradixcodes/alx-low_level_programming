9b164e63025e545b40ee1f468aca8127a4ca194d
Tradix <tradix2021@gmail.com>
2023-09-21 11:45:11 -0700

    strncat ii

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,7 +10,7 @@
  *
  * Return: concatenated string dest
  */
-char *_strncat(chra *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	strncat(dest, src, n);
 	return (dest);
