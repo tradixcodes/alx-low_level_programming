2bc6e38c40fd2da67e0301c529dd95a8ebd5021f
Tradix <tradix2021@gmail.com>
2023-09-21 12:00:22 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compares strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 - identical, more or less - not identical
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result = strcmp(s1, s2);
+
+	return (result);
+}
