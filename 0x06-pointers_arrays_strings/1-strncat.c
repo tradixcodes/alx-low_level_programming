5f03365bb29ccf2d8e8b2abc515e812098445acf
Tradix <tradix2021@gmail.com>
2023-09-21 11:43:43 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - concatenates(combines)
+ * two strings but has flexibility of determining
+ * how many bytes from src will be concatenated
+ * @dest: destination string
+ * @src: source string
+ * @n: byte input
+ *
+ * Return: concatenated string dest
+ */
+char *_strncat(chra *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
