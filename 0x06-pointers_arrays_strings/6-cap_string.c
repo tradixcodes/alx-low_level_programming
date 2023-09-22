4e543717e831b582c1055db1e571243a70f90db3
Tradix <tradix2021@gmail.com>
2023-09-21 19:47:29 -0700

    Trials

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - capitalizes strings
+ * i don't see the difference with upper
+ * @c: string parameter
+ *
+ * Return: the capitalized string
+ */
+char *cap_string(char *c)
+{
+	int i;
+
+	i = 0;
+	while (c[i] != '\0')
+	{
+		if (c[i] == ',' || c[i] == ';' || c[i] == '.' || c[i] == '?' || c[i] == '"' || c[i] == '(' || c[i] == ')' || c[i] == '{' || c[i] == '}')
+		{
+			int next;
+
+			next = i + 1;
+			if (c[next] >= 'a' && c[next] <= 'z')
+			{
+				c[next] -= 32;
+			}
+			i++;
+		}
+	}
+	printf("\n");
+	return (c);
+}
