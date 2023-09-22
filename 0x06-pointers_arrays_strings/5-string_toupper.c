161c471d5aabab0a684b822081ddd2123db84bd4
Tradix <tradix2021@gmail.com>
2023-09-21 19:22:37 -0700

    to upper II

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,22 @@
 #include "main.h"
-
+/**
+ * string_toupper - converts strings to uppercase
+ *
+ * @c: string parameter
+ *
+ * Return: the converted uppercase array
+ */
 char *string_toupper(char *c)
 {
-    int i = 0;
+	int i = 0;
 
-    while (c[i] != '\0')
-    {
-        if (c[i] >= 'a' && c[i] <= 'z')
-        {
-            c[i] = c[i] - 32; // Convert to uppercase
-        }
-        i++;
-    }
-
-    return c;
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] = c[i] - 32;
+		}
+		i++;
+	}
+	return (c);
 }
-
