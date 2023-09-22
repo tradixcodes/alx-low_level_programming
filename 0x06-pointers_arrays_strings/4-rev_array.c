0c867114401b0c75b20e563b7514c1edd149a1c4
Tradix <tradix2021@gmail.com>
2023-09-21 19:57:15 -0700

    mbogi

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,27 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * reverse_array - function that prints arrays
- * inversely
- * @a: array parameter
- * @n: no of items in the array
- * Return: void
- */
+ * reverse_array - a function that reverses the
+ *                content of an array of integers
+ *
+ * @a: pointer to int array
+ * @n: is the number of elements to swap
+ *
+ * Return: nothing
+*/
+
 void reverse_array(int *a, int n)
 {
-	int i;
+	int temp, s, e;
 
-	for (i = n; i > 0; i--)
+	s = 0;
+	e = n - 1;
+	/**
+	 * set value in array a in temp
+	 * then place the last array in
+	 * the first array then place
+	 * value in temp to last array
+	*/
+	while (s < e)
 	{
-		if (i != n)
-		{
-			printf(", ");
-			printf("%d", a[i]);/*	*/
-		}
-		else/* */
-		{
-			printf("%d", a[i]);
-		}
+		temp = a[s];
+		a[s] = a[e];
+		a[e] = temp;
+		s++;
+		e--;
 	}
-	printf("\n");
 }
