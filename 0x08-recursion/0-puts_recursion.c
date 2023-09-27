02c4cf11da03ef64795da059350226a8b843cb7f
Tradix <tradix2021@gmail.com>
2023-09-27 03:33:46 -0700

    Recursion I

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string by calling itself
+ *
+ * @s: parameter for a string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
