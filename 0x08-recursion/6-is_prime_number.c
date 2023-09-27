ab2890b5dcd323d83586cf5688527b6d90afb604
Tradix <tradix2021@gmail.com>
2023-09-27 13:10:08 -0700

    5 & 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@ int is_prime_number(int n)
 {
 	if (n > 1)
 	{
-		if ((n / 2 != 0) && (n / 3 != 0) && (n / 5 != 0) && (n / 11 != 0))
+		if ((n % 2 != 0) && (n % 3 != 0) && (n % 5 != 0))
 		{
 			return (n);
 		}
