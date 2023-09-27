3da792fced9e61ea51bf6ec24256d349e21ef809
Tradix <tradix2021@gmail.com>
2023-09-27 04:23:21 -0700

    Working trial

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,48 +1,18 @@
 #include "main.h"
-#include <string.h>
-/**_print_rev_recursion - prints string in reverse
- * using recursion
- * @s: string value parameter
+
+/**
+ * _print_rev_recursion - prints a string in reverse
  *
- * Return: void
- */
+ * @s: string
+ *
+ * Return: nothing
+*/
+
 void _print_rev_recursion(char *s)
 {
-	int n = strlen(s);
-	int i;
-
-	if (n == '\0')
+	if (*s != '\0')
 	{
-	}
-	if (i == '\0')
-	{
-	}
-	if (n % 2 == 0)
-	{
-		for (i = 0; i < (n / 2); i++)
-		{
-			int temp;
-
-			temp = s[n];
-			s[n] = s[i];
-			s[i] = temp;
-			_print_rev_recursion(s[n - 1]);
-			_putchar(s)
-		}
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < ((n + 1) / 2)); i++)
-		{
-			int temp;
-
-			temp = s[n];
-			s[n] = s[i];
-			s[i] = temp;
-			_print_rev_recursion(s[n - 1]);
-			_putchar(s);
-		}
-		_putchar('\n');
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
